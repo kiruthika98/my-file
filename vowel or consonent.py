338e0106c9bec8124cd8b0122bc26847232a300a
kiruthika98 <36496769+kiruthika98@users.noreply.github.com>
2018-02-15 14:42:44 +0530

    vowel or consonent

---
+++ vowel or consonent.py
@@ -0,0 +1,6 @@
+ch = raw_input("Enter any character: ")
+if(ch=='a' or ch=='A' or ch=='e' or ch=='E' or ch=='i' or ch=='I'
+		   or ch=='o' or ch=='O' or ch=='u' or ch=='U'):
+			print(ch, "is a vowel")
+		else:
+			print(ch, "is a consonent")
